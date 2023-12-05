5fd56cdf685c2e6797ec222262277650845354ec
Nguyen Nhat Long <long.nguyen2198@gmail.com>
2023-12-05 07:00:38 +0700

    update rate

--- public/manager/assets/js/page/warehouse.js
+++ public/manager/assets/js/page/warehouse.js
@@ -356,7 +356,7 @@ const View = {
     function getProduct(){
         Api.Product.GetAll()
             .done(res => {
-                View.product = res.data.data; 
+                View.product = res.data; 
             })
             .fail(err => { IndexView.helper.showToastError('Error', 'Có lỗi sảy ra'); })
             .always(() => { });
